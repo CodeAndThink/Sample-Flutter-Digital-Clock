6f4bb7710b86d9b22304513fa4267e7eee37b944
Truong Ngo <trg1432001.tu@gmail.com>
2024-11-18 10:32:05 +0700

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# digital_clock
+
+A new Flutter project.
---
+++ lib/views/home.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+import 'package:digital_clock/views/custome_view.dart/lcd_led.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var n = ' ';
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (context.mounted) {
+        setState(() {
+          var now = DateTime.now();
+          n = DateFormat('kk:mm:ss').format(now);
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: lcdClock(),
+      ),
+    );
+  }
+
+  Widget lcdClock() {
+    return SizedBox(
+            height: 70,
+            child: AspectRatio(
+              aspectRatio: n.length.toDouble() / 1.5,
+              child: LedDigits(
+                string: n,
+                spacing: 12,
+                numberOfLeds: n.length,
+                onColor: const Color(0xFF171717),
+                backgroundColor: Colors.transparent,
+                offColor: const Color(0xFFE6E6E6) ,
+              ),
+            ),
+          );
+  }
+}
